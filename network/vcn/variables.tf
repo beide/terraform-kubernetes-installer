30a1c6033f22b3eb938a04e1565d83b0b66c1a41
BDW <33850904+beide@users.noreply.github.com>
2018-04-28 21:22:35 -0400

    Update variables.tf

--- network/vcn/variables.tf
+++ network/vcn/variables.tf
@@ -1,7 +1,13 @@
 variable "vcn_dns_name" {
- default = "k8sbmcs"
- }
+  default = "k8sbmcs"
+}
+
 variable "create_vcn" {}
+
 variable "vcn_cidr" {
-   default = "10.0.0.0/16"
- }
+  default = "10.0.0.0/16"
+}
+
+variable "compartment_ocid" {}
+
+variable "label_prefix" {}
