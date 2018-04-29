b176cc8e3c4a22a91195a6bb3867f8dbde04818f
BDW <33850904+beide@users.noreply.github.com>
2018-04-28 21:30:35 -0400

    Update routes.tf

--- network/subnets/routes.tf
+++ network/subnets/routes.tf
@@ -1,10 +1,10 @@
 resource "oci_core_route_table" "NATInstanceAD1RouteTable" {
-+  # Provisioned only when k8s instances in AD1 are in private subnets
-+  count          = "${(var.control_plane_subnet_access == "private") && (var.nat_instance_ad1_enabled == "true") ? "1" : "0"}"
-+  compartment_id = "${var.compartment_ocid}"
-+  vcn_id         = "${var.vcn_id}"
-+  display_name   = "NATInstanceAD1RouteTable"
-+
+  # Provisioned only when k8s instances in AD1 are in private subnets
+  count          = "${(var.control_plane_subnet_access == "private") && (var.nat_instance_ad1_enabled == "true") ? "1" : "0"}"
+  compartment_id = "${var.compartment_ocid}"
+  vcn_id         = "${var.vcn_id}"
+  display_name   = "NATInstanceAD1RouteTable"
+
   route_rules {
     # All traffic leaving the subnet needs to go to route target.
     cidr_block = "0.0.0.0/0"
@@ -17,7 +17,7 @@ resource "oci_core_route_table" "NATInstanceAD1RouteTable" {
 resource "oci_core_route_table" "NATInstanceAD2RouteTable" {
   # Provisioned only when k8s instances in AD2 are in private subnets
   count          = "${(var.control_plane_subnet_access == "private") && (var.nat_instance_ad2_enabled == "true") ? "1" : "0"}"
- compartment_id = "${var.compartment_ocid}"
+  compartment_id = "${var.compartment_ocid}"
   vcn_id         = "${var.vcn_id}"
   display_name   = "NATInstanceAD2RouteTable"
 
@@ -41,7 +41,7 @@ resource "oci_core_route_table" "NATInstanceAD3RouteTable" {
     # All traffic leaving the subnet needs to go to route target.
     cidr_block = "0.0.0.0/0"
 
-   # Private IP route target for instances on private AD3 subnets
+    # Private IP route target for instances on private AD3 subnets
     network_entity_id = "${data.oci_core_private_ips.NATInstanceAD3PrivateIPDatasource.private_ips.0.id}"
   }
 }
